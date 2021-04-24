bfb8e7399e7aacd73be1356142d49fd2b4068236
Danilo Kovačević <kovacevic.danilo2003@gmail.com>
2021-04-24 19:54:28 +0200

    Update

---
+++ Assets/ScriptsNew/Enemy/EnemyPlane.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPlane : MonoBehaviour, IEnemy, IPlaneController
+{
+    [SerializeField]PlaneMovement planeController;
+    GameObject target;
+    Vector3 attackRadius;
+    public void SetTarget(GameObject target)
+    {
+        this.target = target;
+    }
+    public void SetAttackRange(Vector3 radius)
+    {
+        attackRadius = radius;
+    }
+    public void Attack()
+    {
+        //PUC PUC
+    }
+    public void SpeedUp(int value)
+    {
+        planeController.SpeedUp(1);
+    }
+    public void SlowDown(int value)
+    {
+        planeController.SlowDown(1);
+    }
+    public void Pitch(float value)
+    {
+        planeController.Pitch(value);
+    }
+    public void Roll(float value)
+    {
+        planeController.Roll(value);
+    }
+    public void Yaw(float value)
+    {
+        planeController.Yaw(value);
+    }
+}
---
+++ Assets/ScriptsNew/Enemy/IEnemy.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+public interface IEnemy
+{
+    void SetTarget(GameObject target);
+    void SetAttackRange(Vector3 radius);
+    void Attack();
+}
